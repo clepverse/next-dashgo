9609e9863fc923793063328d35d195cb483d84fa
clepverse <cleepe5@hotmail.com>
2022-06-11 14:23:06 -0300

    Componente SignIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
+import { Button, Flex, Stack } from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
+
+export default function SignIn() {
   return (
-    <div>
-      <h1>Hello, Dashgo!</h1>
-    </div>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <Input name="email" type="email" label="E-mail" />
+
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
